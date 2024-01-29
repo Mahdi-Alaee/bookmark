38e2be5bacf6513ad79be4817aadcb04946ca2ae
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-01-29 14:27:38 +0330

    coding html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/header";
+
 function App() {
   return (
     <>
-      <h1 className="text-center text-4xl py-4 bg-red-700 text-white">hi there</h1>
+      <Header />
     </>
   );
 }
