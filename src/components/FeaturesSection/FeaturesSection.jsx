05616ee880480f9c9ef806f17be00e01fe79dbc7
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-01-29 18:18:19 +0330

    coding FeaturesSection Styles

--- src/components/FeaturesSection/FeaturesSection.jsx
+++ src/components/FeaturesSection/FeaturesSection.jsx
@@ -2,34 +2,44 @@ import Feature from "./components/Feature";
 
 function FeaturesSection() {
   return (
-    <section>
+    <section className="mt-20">
       {/* container */}
-      <div className="container mx-auto px-6">
+      <div className="container mx-auto px-6 text-center">
         {/* title */}
-        <h2>Features</h2>
+        <h2 className="text-4xl font-bold">Features</h2>
 
         {/* description */}
-        <p>
+        <p className="mt-8 mb-16 text-grayishBlue max-w-md mx-auto">
           Our aim is to make it quick and easy for you to access your favourite
           websites. Your bookmarks sync between your devices so you can access
           them on the go.
         </p>
 
-        {/* content */}
-        <div>
-          {/* menu */}
-          <ul>
-            {/* item 1 */}
-            <li>Simple Bookmarking</li>
-            {/* item 2 */}
-            <li>Speedy Searching</li>
-            {/* item 3 */}
-            <li>Easy Sharing</li>
-          </ul>
+        {/* menu */}
+        <ul className="flex flex-col mb-10 border-gray-300 md:flex-row md:gap-x-20 
+        md:border-b md:w-max md:mx-auto md:px-2">
+          {/* item 1 */}
+          <li className="text-gray-600 hover:text-softRed border-b border-gray-300 md:border-b-0">
+            <a className="border-b-4 border-softRed py-5 inline-block" href="#">
+              Simple Bookmarking
+            </a>
+          </li>
+          {/* item 2 */}
+          <li className="text-gray-600 hover:text-softRed border-b border-gray-300 md:border-b-0">
+            <a className="border-softRed py-5 inline-block" href="#">
+              Speedy Searching
+            </a>
+          </li>
+          {/* item 3 */}
+          <li className="text-gray-600 hover:text-softRed border-b border-gray-300 md:border-b-0">
+            <a className="border-softRed py-5 inline-block" href="#">
+              Easy Sharing
+            </a>
+          </li>
+        </ul>
 
-          {/* feature */}
-          <Feature />
-        </div>
+        {/* feature */}
+        <Feature />
       </div>
     </section>
   );
--- src/components/FeaturesSection/components/Feature.jsx
+++ src/components/FeaturesSection/components/Feature.jsx
@@ -1,23 +1,24 @@
 function Feature() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-28 md:flex-row md:gap-x-6">
       {/* banner */}
-      <img src="/images/illustration-features-tab-1.svg" alt="feature image" />
+      <img className="md:w-1/2" src="/images/illustration-features-tab-1.svg" alt="feature image" />
 
       {/* content */}
-      <div>
+      <div className="md:w-1/2 md:text-left">
         {/* title */}
-        <h3>Bookmark in one click</h3>
+        <h3 className="text-3xl font-bold">Bookmark in one click</h3>
 
         {/* description */}
-        <p>
+        <p className="max-w-md text-grayishBlue mx-auto mt-10 mb-8">
           Organize your bookmarks however you like. Our simple drag-and-drop
           interface gives you complete control over how you manage your
           favourite sites.
         </p>
 
         {/* button */}
-        <a href="#">More Info</a>
+        <a className="inline-block px-6 py-2 bg-softBlue text-white border-2 border-softBlue
+         rounded-md font-bold tracking-wider hover:bg-transparent hover:text-softBlue" href="#">More Info</a>
       </div>
     </div>
   );
