8b0a29e5ecd4b94cb561f6c538c7bfb92defc02f
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-01-30 17:22:51 +0330

    coding ExtentionSection html structure

--- src/components/ExtentionSection/ExtentionSection.jsx
+++ src/components/ExtentionSection/ExtentionSection.jsx
@@ -1,9 +1,23 @@
 function ExtentionSection() {
-    return ( 
-        <section>
-        
-        </section>
-     );
+  return (
+    <section className="mt-20">
+      <div className="container max-auto px-6">
+        {/* title */}
+        <h2>Download the extension</h2>
+
+        {/* description */}
+        <p>
+          We've got more browsers in the pipeline. Please do let us know if
+          you've got a favourite you'd like us to prioritize.
+        </p>
+
+        {/* content */}
+        <div>
+            
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ExtentionSection;
