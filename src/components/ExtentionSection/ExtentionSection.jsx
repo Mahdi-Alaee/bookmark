f7ba43507d28b5c1210719d428bd403d165abab6
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-01-30 18:52:47 +0330

    coding styles of Extention

--- src/components/ExtentionSection/ExtentionSection.jsx
+++ src/components/ExtentionSection/ExtentionSection.jsx
@@ -2,20 +2,22 @@ import ExtentionBox from "./components/ExtentionBox";
 
 function ExtentionSection() {
   return (
-    <section className="mt-20">
-      <div className="container max-auto px-6">
+    <section className="mt-32">
+      <div className="container mx-auto px-6 text-center">
         {/* title */}
-        <h2>Download the extension</h2>
+        <h2 className="text-3xl font-bold mb-6 tracking-wide">Download the extension</h2>
 
         {/* description */}
-        <p>
+        <p className="text-grayishBlue mb-32 max-w-md mx-auto">
           We've got more browsers in the pipeline. Please do let us know if
           you've got a favourite you'd like us to prioritize.
         </p>
 
         {/* content */}
-        <div>
-            <ExtentionBox />
+        <div className="grid grid-cols-1 gap-8 px-4 max-w-5xl md:grid-cols-3 md:px-0 md:-mx-5 lg:mx-auto">
+            <ExtentionBox title='Add to Chrome' image='logo-chrome.svg' version='Minimum Version 62' />
+            <ExtentionBox className='mt-4 -mb-4' title='Add to Firefox' image='logo-firefox.svg' version='Minimum Version 55' />
+            <ExtentionBox className='mt-8 -mb-8' title='Add to Opera' image='logo-opera.svg' version='Minimum Version 46' />
         </div>
       </div>
     </section>
--- src/components/ExtentionSection/components/ExtentionBox.jsx
+++ src/components/ExtentionSection/components/ExtentionBox.jsx
@@ -1,21 +1,29 @@
-function ExtentionBox() {
-    return ( 
-        <div>
-            {/* image */}
-            <img className="mb-12" src="/images/logo-chrome.svg" alt="browser logo" />
+function ExtentionBox({ image, title, version, className }) {
+  return (
+    <div className={`flex flex-col items-center shadow-lg rounded-lg ${className}`}>
+      {/* top content */}
+      <div className="flex flex-col items-center py-6">
+        {/* image */}
+        <img className="mb-12" src={`/images/${image}`} alt="browser logo" />
 
-            {/* title */}
-            <h5>Add to Chrome</h5>
+        {/* title */}
+        <h5 className="font-bold text-xl">{title}</h5>
 
-            {/* version */}
-            <p>Minimum Version 62</p>
+        {/* version */}
+        <p className="text-grayishBlue mt-4">{version}</p>
+      </div>
 
-            {/* dots & button */}
-            <div>
-                <a href="#">Add & Install Extension</a>
-            </div>
-        </div>
-     );
+      {/* dots & button */}
+      <div className="bg-dots p-6 w-full">
+        <a
+          className="block bg-softBlue text-white py-3 rounded-lg border-2 border-softBlue duration-200 hover:bg-transparent hover:text-softBlue"
+          href="#"
+        >
+          Add & Install Extension
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default ExtentionBox;
