609a3b8fc8eee82d4d211b24a6a108f2d9f460d2
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-01-31 17:30:23 +0330

    coding the html structure of NewsLetter

---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,24 @@
+function NewsLetter() {
+  return (
+    <section>
+      {/* container */}
+      <div>
+        {/* top small text */}
+        <p>35,000+ ALREADY JOINED</p>
+
+        {/* title */}
+        <h6>Stay up-to-date with what we're doing</h6>
+
+        {/* form */}
+        <form>
+          {/* textbox */}
+          <input type="text" placeholder="Enter your email address" />
+          {/* submit button */}
+          <button type="submit">Contact Us</button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default NewsLetter;
